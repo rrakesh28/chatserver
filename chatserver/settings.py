9ba4629b54cfa27362c2e7d7c45267cea8c5b355
r-rakesh28 <rebbavarapurakesh@gmail.com>
2021-04-26 15:06:52 +0530

    a

--- chatserver/settings.py
+++ chatserver/settings.py
@@ -138,7 +138,7 @@ MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static_cdn')
 MEDIA_ROOT = os.path.join(BASE_DIR,'media_cdn')
 
-TEMP = os.path.join(BASE_DIR,'./temp')
+TEMP = os.path.join(BASE_DIR,'/temp')
 
 STATIC_URL = '/static/'
 
