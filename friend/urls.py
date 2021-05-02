34d8e045d7e3d32d84bebf9b30bf03260cfb3569
r-rakesh28 <rebbavarapurakesh@gmail.com>
2021-05-02 20:31:03 +0530

    commit

--- friend/urls.py
+++ friend/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from friend.views import (
 	send_friend_request,
+	friend_requests,
 )
 
 app_name = 'friend'
 
 urlpatterns = [
-	path('friend_request/',send_friend_request,name='friend_request')
+	path('friend_request/',send_friend_request,name='friend_request'),
+	path('friend_request/<user_id>/',friend_requests,name='friend_request')
 ]
