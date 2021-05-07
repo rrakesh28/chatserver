8c4ab403516644add5e570ef95173ca7e6478827
r-rakesh28 <rebbavarapurakesh@gmail.com>
2021-05-07 12:54:30 +0530

    show password

--- post/views.py
+++ post/views.py
@@ -16,12 +16,14 @@ def add_post(request, *args, **kwargs):
 	context = {}
 	user = request.user
 
-	if request.method == "POST":
-		content = request.POST['post_data']
-		post = Post(creator=user, content=content)
-		post.save()
-	return render(request,'post/add_post_page.html',context)
-
+	if user.is_authenticated: 
+		if request.method == "POST":
+			content = request.POST['post_data']
+			post = Post(creator=user, content=content)
+			post.save()
+			return redirect('home')
+	else:
+		return redirect('login')
 
 
 
